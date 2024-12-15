b830ad54c4e0685264ea1dc68b00bafd7ea0276b
Jordan Campbell <jordancampbell.work@gmail.com>
2024-12-15 13:47:05 -0500

    redirects to users.php when success of user creation

--- public/newuser.php
+++ public/newuser.php
@@ -37,7 +37,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $stmt->execute();
 
         // Redirect or display success message
-        header("Location: ../php/API/userList.php?success=1");
+        header("Location: users.php");
         exit();
     } catch (PDOException $e) {
         echo "Error: " . $e->getMessage();
