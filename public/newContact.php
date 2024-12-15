eff79561ca7c8e8653b0239d1b18c0a740531350
Jordan Campbell <jordancampbell.work@gmail.com>
2024-12-14 23:43:28 -0500

    re-added grid layout to new contact.php after merging issue

--- public/newContact.php
+++ public/newContact.php
@@ -83,15 +83,18 @@ try {
             border-radius: 8px;
             box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
         }
+
         #grid-container {
             display: grid;
             grid-template-columns: auto auto;
             grid-template-rows: auto auto auto auto auto auto;
             column-gap: 3em;
         }
+
         #title {
             width: auto;
         }
+
         #assigned_to {
             width: auto;
         }
@@ -150,46 +153,58 @@ try {
             <h1>New Contact</h1>
 
             <div class="form-container">
-            <form action="../php/API/CreateContact.php" method="POST">
-            <label for="title">Title:</label>
-            <select name="title" id="title" required>
-                <option value="Mr">Mr</option>
-                <option value="Ms">Ms</option>
-                <option value="Mrs">Mrs</option>
-            </select><br>
+                <form action="../php/API/CreateContact.php" method="POST">
+                    <label for="title">Title:</label>
+                    <select name="title" id="title" required>
+                        <option value="Mr">Mr</option>
+                        <option value="Ms">Ms</option>
+                        <option value="Mrs">Mrs</option>
+                    </select><br>
 
-            <label for="firstname">First Name:</label>
-            <input type="text" name="firstname" id="firstname" required><br>
+                    <div id="grid-container">
+                        <div>
+                            <label for="firstname">First Name:</label>
+                            <input type="text" name="firstname" id="firstname" required><br>
+                        </div>
 
-            <label for="lastname">Last Name:</label>
-            <input type="text" name="lastname" id="lastname" required><br>
+                        <div>
+                            <label for="lastname">Last Name:</label>
+                            <input type="text" name="lastname" id="lastname" required><br>
+                        </div>
 
-            <label for="email">Email:</label>
-            <input type="email" name="email" id="email" required><br>
+                        <div>
+                            <label for="email">Email:</label>
+                            <input type="email" name="email" id="email" required><br>
+                        </div>
+                        <div>
+                            <label for="telephone">Telephone:</label>
+                            <input type="text" name="telephone" id="telephone"><br>
+                        </div>
+                        <div>
+                            <label for="company">Company:</label>
+                            <input type="text" name="company" id="company"><br>
+                        </div>
+                        <div>
+                            <label for="type">Type:</label>
+                            <select name="type" id="type" required>
+                                <option value="sales lead">Sales Lead</option>
+                                <option value="support">Support</option>
+                            </select><br>
+                        </div>
 
-            <label for="telephone">Telephone:</label>
-            <input type="text" name="telephone" id="telephone"><br>
+                    </div>
 
-            <label for="company">Company:</label>
-            <input type="text" name="company" id="company"><br>
+                    <label for="assigned_to">Assigned To:</label>
+                    <select name="assigned_to" id="assigned_to" required>
+                        <?php foreach ($users as $user): ?>
+                            <option value="<?= $user['id'] ?>"><?= $user['firstname'] . ' ' . $user['lastname'] ?></option>
+                        <?php endforeach; ?>
+                    </select><br>
 
-            <label for="type">Type:</label>
-            <select name="type" id="type" required>
-                <option value="sales lead">Sales Lead</option>
-                <option value="support">Support</option>
-            </select><br>
-
-            <label for="assigned_to">Assigned To:</label>
-            <select name="assigned_to" id="assigned_to" required>
-                <?php foreach ($users as $user): ?>
-                    <option value="<?= $user['id'] ?>"><?= $user['firstname'] . ' ' . $user['lastname'] ?></option>
-                <?php endforeach; ?>
-            </select><br>
-
-            <div id="sbt-bt-container">
-                <button type="submit">Add Contact</button>
-            </div>
-        </form>
+                    <div id="sbt-bt-container">
+                        <button type="submit">Add Contact</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
