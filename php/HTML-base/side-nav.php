1898173de4de50fc2421e86d8d896c34d5bfea5e
Jordan Campbell <jordancampbell.work@gmail.com>
2024-12-16 13:24:41 -0500

    removed title of sidenav so that dynamically loaded pages title show correectly

--- php/HTML-base/side-nav.php
+++ php/HTML-base/side-nav.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../public/css/styles-index.css">
-    <title>Side Navigation</title>
     <style>
         /* Basic styles for the side navigation */
         .sidenav img{
