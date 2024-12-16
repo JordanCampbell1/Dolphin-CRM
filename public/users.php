b7040d8d3ddb96da1138a276a76e7eee0a4e31f5
Jordan Campbell <jordancampbell.work@gmail.com>
2024-12-16 13:08:16 -0500

    side nav works completely in dynamic loading while keeping the hover effects

--- public/users.php
+++ public/users.php
@@ -24,62 +24,42 @@ try {
     exit();
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <?php include '../php/HTML-base/head.php'; ?>
-    <title>Users - Dolphin CRM</title>
-    <link rel="stylesheet" href="css/styles-index.css" />
-    <link rel="stylesheet" href="css/users.css">
-</head>
-<body>
-    <div class="top">
-        <?php include '../php/HTML-base/navbar.php'; ?>
-    </div>
 
-    <div class="main-container">
-        <div class="side">
-            <?php include '../php/HTML-base/side-nav.php'; ?>
-        </div>
 
-        <div class="content">
-            <div class="content-header">
-                <h1>Users</h1>
-                <a href="newuser.php" class="add-user-btn">Add User</a>
-            </div>
 
-            <div class="table-container">
-                <table class="users-table">
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Role</th>
-                            <th>Created At</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        if (count($users) > 0) {
-                            foreach ($users as $user) {
-                                echo "<tr>";
-                                echo "<td>{$user['firstname']} {$user['lastname']}</td>";
-                                echo "<td>{$user['email']}</td>";
-                                echo "<td>{$user['role']}</td>";
-                                echo "<td>{$user['created_at']}</td>";
-                                echo "</tr>";
-                            }
-                        } else {
-                            echo "<tr><td colspan='4'>No users found.</td></tr>";
-                        }
-                        ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
 
-</body>
-</html>
+<title>Users - Dolphin CRM</title>
+<link rel="stylesheet" href="css/users.css">
+<div class="content-header">
+    <h1>Users</h1>
+    <a href="newuser.php" class="add-user-btn">Add User</a>
+</div>
 
-</html>
+<div class="table-container">
+    <table class="users-table">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Role</th>
+                <th>Created At</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            if (count($users) > 0) {
+                foreach ($users as $user) {
+                    echo "<tr>";
+                    echo "<td>{$user['firstname']} {$user['lastname']}</td>";
+                    echo "<td>{$user['email']}</td>";
+                    echo "<td>{$user['role']}</td>";
+                    echo "<td>{$user['created_at']}</td>";
+                    echo "</tr>";
+                }
+            } else {
+                echo "<tr><td colspan='4'>No users found.</td></tr>";
+            }
+            ?>
+        </tbody>
+    </table>
+</div>
